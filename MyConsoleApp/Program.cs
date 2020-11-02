ea98ad45437a048580a07d50cc7442b1d6b4e997
Dmitry998 <40994829+Dmitry998@users.noreply.github.com>
2020-11-02 22:24:16 +0300

    add A class

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -2,11 +2,25 @@
 
 namespace MyConsoleApp
 {
+    class A
+    {
+        private int _a = 4;
+        private int _b = 5;
+
+        public int Method()
+        {
+            return _a + _b;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            A my_a = new A();
+            Console.WriteLine(my_a.Method());
             Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
